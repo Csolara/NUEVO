8c8c93b812f84b7024af672bd7e2d20f17d57088
DESKTOP-5BAJO87\Profesor <csi23-csolara@colegioaltair.net>
2023-09-26 14:15:39 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,64 @@
+﻿using NUEVO.Servicios;
+
+namespace NUEVO.controladores
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// 250923 - csl
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método de entrada de la aplicación
+        /// 250923 - csl
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            MenuInterfaz mi = new MenuImplementacion();
+            mi.mostrarMensajeBienvenida();
+
+            //variable que controla la entrada y salida del blucle while
+            bool cerrarMenu = false;
+            //contener la opción del usuario
+            int opcionSeleccionada;
+
+            //desde la primera iteraccion debe cumplirse la condición
+            while (!cerrarMenu)
+            {
+                opcionSeleccionada = mi.mostrarMenuYSeleccion();
+
+                switch (opcionSeleccionada)
+                {
+                    case 0:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 0");
+                        cerrarMenu = true; 
+                        break;
+                    case 1:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 4");
+                        break;
+                    case 5:
+                        Console.WriteLine("[INFO] - Se ejecuta caso 5");
+                        break;
+                    default:
+                        Console.WriteLine("[INFO] - La opcion seleccionada no esta coincide con ninguna");
+                        break;
+                }
+
+            }
+
+        }
+        
+    }
+
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUEVO.Servicios
+{
+    /// <summary>
+    /// Interfaz con la relación de métodos de la funcionalida menú
+    /// 250923 - csl
+    /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// Método quemuestra mensaje de bienvenida al cajero
+        /// 250923 - csl
+        /// </summary>
+        public void mostrarMensajeBienvenida();
+
+        /// <summary>
+        /// Método que muestra el menú y recoge la selección del usuario
+        /// 260923 - csl
+        /// </summary>
+        /// <returns></returns>
+        public int mostrarMenuYSeleccion();
+
+
+
+    }
+}
